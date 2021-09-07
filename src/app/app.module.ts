15fd8366ddb619a4614670d9545b39df01532c41
Daniel <danielakanbi01@gmail.com>
2021-09-06 18:33:08 -0700

    written

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductComponent } from './product/product.component';
+import { ServiceComponent } from './service/service.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    ProductComponent,
+    ServiceComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
